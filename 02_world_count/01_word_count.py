7e6bef269d6d999c70fcc07361f21cf1bdea09ed
jakub <jakubbandosz@gazeta.pl>
2022-11-03 16:27:40 +0100

    update

--- 02_world_count/01_word_count.py
+++ 02_world_count/01_word_count.py
@@ -1,9 +1,14 @@
-from mrjob import MRJob
+from mrjob.job import MRJob
 from mrjob.step import MRStep
 
-class MRWordCount(MRJob)
+class MRWordCount(MRJob):
+
     def mapper(self, _, line):
-        yield line,1
+        words = line.split()
+        for word in words:
+            yield word.lower(),1
+    def reducer(self, key, values):
+        yield key, sum(values)
 
 if __name__ == '__main__':
     MRWordCount.run()
