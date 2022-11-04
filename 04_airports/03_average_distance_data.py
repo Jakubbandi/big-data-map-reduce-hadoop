df4e16efb423c959da067247e5b5c169c4204de2
jakub <jakubbandosz@gazeta.pl>
2022-11-04 17:43:52 +0100

    update

--- 04_airports/03_average_distance_data.py
+++ 04_airports/03_average_distance_data.py
@@ -5,7 +5,8 @@ class MRFlights(MRJob):
 
     def steps(self):
         return[
-            MRStep(mapper=self.mapper)
+            MRStep(mapper=self.mapper,
+                   reducer=self.reducer)
         ]
 
 
@@ -13,7 +14,17 @@ class MRFlights(MRJob):
         year, items = line.split('\t')
         year = year[1:-1]
         items = items[1:-1]
-        yield year, items
+        month, day, airline, distance = items.split(', ')
+        distance = int(distance)
+        yield None, distance
+
+    def reducer(self, key, values):
+        total = 0
+        num_elements = 0
+        for value in values:
+            total += value
+            num_elements += 1
+        yield None, total/num_elements
 
 
 if __name__ == '__main__':
