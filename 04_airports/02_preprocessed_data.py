a51008a5aa025561624ba8991d27de08b5067397
jakub <jakubbandosz@gazeta.pl>
2022-11-04 16:51:21 +0100

    update

--- 04_airports/02_preprocessed_data.py
+++ 04_airports/02_preprocessed_data.py
@@ -1,7 +1,7 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 
-class MRFlights(MRJob):
+class MRPreprocess(MRJob):
 
     def mapper(self, _, line):
         (year, month, day, day_of_week, airline, flight_number, tail_number, origin_airport,
@@ -9,10 +9,9 @@ class MRFlights(MRJob):
          wheels_off, scheduled_time, elapsed_time, air_time, distance, wheels_on, taxi_in,
          scheduled_arrival, arrival_time, arrival_delay, diverted, cancelled, cancellation_reason,
          air_system_delay, security_delay, airline_delay, late_aircraft_delay, weather_delay) = line.split(',')
-        yield 'row',1
+        yield year, (month,day,airline,distance)
 
-    def reducer(self, key, values):
-        yield key, sum(values)
+
 
 if __name__ == '__main__':
     MRFlights.run()
