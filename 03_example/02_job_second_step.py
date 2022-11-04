21c19d3b49b01b5fbd8d0df88c747e8d9f8f824f
jakub <jakubbandosz@gazeta.pl>
2022-11-04 08:41:21 +0100

    update

--- 03_example/02_job_second_step.py
+++ 03_example/02_job_second_step.py
@@ -10,7 +10,9 @@ class MRJobFirstStep(MRJob):
         return[
             MRStep(mapper=self.mapper,
                    combiner=self.combiner,
-                   reducer=self.reducer)
+                   reducer=self.reducer),
+            MRStep(mapper=self.mapper_get_keys,
+                   reducer=self.reducer_find_most_common_word)
         ]
     def mapper(self, _, line):
         words = WORD_RE.findall(line)
@@ -22,6 +24,13 @@ class MRJobFirstStep(MRJob):
 
     def reducer(self, key, values):
         yield key, sum(values)
+
+    def mapper_get_keys(self, key,value):
+        yield None, (value, key)
+
+    def reducer_find_most_common_word(self, key, values):
+        yield max(values)
+
 
 if __name__ == '__main__':
     MRJobFirstStep.run()
