efb756ac4535c46fe59fed9524cfdb61b1f15f08
jakub <jakubbandosz@gazeta.pl>
2022-11-08 08:50:12 +0100

    update

--- 05_taxi/03_most_popular_pick_up_location.py
+++ 05_taxi/03_most_popular_pick_up_location.py
@@ -1,16 +1,17 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 
-class MRMap(MRJob):
+class MRTaxi(MRJob):
+
+    def steps(self):
+        return[
+            MRStep(mapper=self.mapper)
+        ]
 
     def mapper(self, _, line):
         (VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count, trip_distance, pickup_longitude,
          pickup_latitude, RetacodeID, store_and_fwd_flag, dropoff_longitude, dropoff_latitude, payment_type,
          fare_amount, extra, mta_tax, tip_amount, tolls_amount, improvement_surcharge, total_amount) = line.split(',')
-        yield passenger_count, 1
+        yield (pickup_latitude, pickup_longitude), 1
 
-    def reducer(self, key, values):
-        yield key, sum(values)
-
-if __name__ == '__main__':
-    MRMap.run()
+if __name__ == '__main__'
